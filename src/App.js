4defe78c04f04b5de5601c7d71e127c715021b3d
Hrycek <HrycekJ@pennmedicine.upenn.edu>
2019-09-17 15:46:23 -0400

    figuring out routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import React from 'react';
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      
+      <Route
+        exact
+        path='/'
+        render={() =>
+          <MainPageCompnent />}
+      />
+      
+      <Route
+        path='/folder/:folder-id'
+        render={() =>
+          <FolderCompnent />}
+      />
+
+      <Route
+        path='/note/:note-id'
+        render={() =>
+          <NoteCompnent />}
+      />
+
     </main>
   );
 }
